8078894ff1a4e6bec45737cb9db2692b4237a10c
jjbarrionuevo19@iespuntadelverde.es <jjbarrionuevo19@iespuntadelverde.es>
2020-10-14 20:54:55 +0200

    COMMIT2

--- src/index.ts
+++ src/index.ts
@@ -6,111 +6,86 @@ const main = async () => {
         n = await menu()
         switch(n){
             case 1:
-                console.log('Ha seleccionado Calcular el área y el perímetro del Rectangulo')
-                let baseR:number, alturar:number
-                baseR = parseInt(await leerTeclado('Porfavor, introduzca la base del rectángulo'))
-                alturar = parseInt(await leerTeclado('Porfavor, introduzca la altura del rectángulo'))
-                let perimetroR=perimetroRectangulo(baseR,alturar)
-                let areaR=areaRectangulo(baseR,alturar)
-                console.log("El perímetro del rectángulo es = "+perimetroR)
-                console.log("El área del rectángulo es = "+areaR)
+                console.log('VAMOS A CALCULAR EL AREA Y PERÍMETRO DEL ROMBO')
+                let diagonalMenor:number, diagonalMayor:number, ladorombo: number
+                diagonalMayor = parseInt(await leerTeclado('INTRODUZCA LA DIAGONAL MAYOR'))
+                diagonalMenor = parseInt(await leerTeclado('INTRODUZCA LA DIAGONAL MENOR'))
+                ladorombo = parseInt(await leerTeclado('INTRODUZCA EL LADO DEL ROMBO'))
+                let perimetrorombo=pRombo(ladorombo)
+                let arearombo=aRombo(diagonalMenor,diagonalMayor,ladorombo)
+                console.log("POR LO TANTO EL PERÍMETRO ES = "+perimetrorombo)
+                console.log("POR LO TANTO ÁREA ES = "+arearombo)
                 break
             case 2:
-                console.log('Ha seleccionado Calcular el área y el perímetro del Cuadrado')
-                let ladoC:number
-                ladoC = parseInt(await leerTeclado('Porfavor, introduzca el lado del cuadrado'))
-                let perimetroC=perimetroCuadrado(ladoC)
-                let areaC=areaCuadrado(ladoC)
-                console.log("El perímetro del cuadrado es = "+perimetroC)
-                console.log("El área del cuadrado es = "+areaC)
+                console.log('VAMOS A CALCULAR EL AREA Y PERÍMETRO DEL CUADRADO')
+                let ladocuadrado:number
+                ladocuadrado = parseInt(await leerTeclado('INTRODUZCA EL LADO DEL CUADRADO'))
+                let perimetrocuadrado=pCuadrado(ladocuadrado)
+                let areacuadrado=aCuadrado(ladocuadrado)
+                console.log("POR LO TANTO EL PERÍMETRO ES = "+perimetrocuadrado)
+                console.log("POR LO TANTO ÁREA ES = "+areacuadrado)
                 break
             case 3:
-                console.log('Ha seleccionado Calcular el área y el perímetro del Triángulo')
-                let baseT:number, alturaT:number, lado1:number, lado2:number
-                baseT =  parseInt( await leerTeclado('Porfavor, introduzca la base del triángulo'))
-                alturaT =  parseInt( await leerTeclado('Porfavor, introduzca la altura del triángulo'))
-                lado1 =  parseInt( await leerTeclado('Porfavor, introduzca otro lado del triángulo'))
-                lado2 =  parseInt( await leerTeclado('Porfavor, introduzca el último lado del triángulo'))
-                let perimetroT=perimetroTriangulo(baseT,lado1,lado2)
-                let areaT=areaTriangulo(baseT,alturaT)
-                console.log("El perímetro del triángulo es = "+perimetroT)
-                console.log("El área del triángulo es = "+areaT)
-                break
-            case 4:
-                console.log('Ha seleccionado Calcular el área y el perímetro del Círculo')
-                let radio:number
-                radio = parseInt(await leerTeclado('Porfavor, introduzca el radio del círculo'))
-                let perimetroCi=perimetroCirculo(radio)
-                let areaCi=areaCirculo(radio)
-                console.log("El perímetro del círculo es = "+perimetroCi)
-                console.log("El área del círculo es = "+areaCi)
+                console.log('VAMOS A CALCULAR EL AREA Y PERÍMETRO DEL RECTÁNGULO')
+                let baserectangulo:number, alturarectangulo:number
+                baserectangulo = parseInt(await leerTeclado('INTRODUZCA EL LADO DEL RECTÁNGULO'))
+                alturarectangulo = parseInt(await leerTeclado('INTRODUZCA LA ALTURA DEL REACTÁNGULO'))
+                let perimetrorectangulo=pRectangulo(baserectangulo,alturarectangulo)
+                let arearectangulo=aRectangulo(baserectangulo,alturarectangulo)
+                console.log("POR LO TANTO EL PERÍMETRO ES = "+perimetrorectangulo)
+                console.log("POR LO TANTO ÁREA ES = "+arearectangulo)
                 break
             case 0:
-                console.log('\nGracias, Adiós')
+                console.log('\nPROCEDEMOS A CERRAR EL PROGRAMA...')
                 break
             default:
-                console.log("Opción incorrecta, por favor use una opción válida")
+                console.log("¿Qué has introducido?")
                 break
         }
     }while (n != 0)
 }
  
 
+/*Calculos rombo */
 
-
-/*Rectángulo */
-
-const perimetroRectangulo = (base:number, altura:number) => {
-    let resRec=0
-    resRec=(base*2+altura*2)
-    return resRec
+const pRombo = (ladorombo:number) => {
+    let cuenta=0;
+    cuenta=ladorombo*4
+    return cuenta
 }
 
-const areaRectangulo = (base:number, altura:number) => {
-    let resR=0
-    resR=altura*base
-    return resR
+const aRombo = (diagonalMenor:number,diagonalMayor:number,ladorombo:number) => {
+    let cuenta=0
+    cuenta=(diagonalMayor*diagonalMenor)/ladorombo
+    return cuenta
+}
+
+/*Calculos cuadrado */
+
+const pCuadrado = (lado:number) => {
+    let cuenta=0
+    cuenta=lado*4
+    return cuenta
+}
+
+const aCuadrado = (lado:number) => {
+    let cuenta=0
+    cuenta=lado*lado
+    return cuenta
+}
+
+/*Calculos rectángulo */
+
+const pRectangulo = (base:number, altura:number) => {
+    let cuenta=0
+    cuenta=(base*2+altura*2)
+    return cuenta
+}
+
+const aRectangulo = (base:number, altura:number) => {
+    let cuenta=0
+    cuenta=altura*base
+    return cuenta
 }
 main()
 
-/*Cuadrado */
-
-const perimetroCuadrado = (lado:number) => {
-    let resCuadrado=0
-    resCuadrado=lado*4
-    return resCuadrado
-}
-
-const areaCuadrado = (lado:number) => {
-    let resC=0
-    resC=lado*lado
-    return resC
-}
-
-/*trángulo */
-
-const perimetroTriangulo = (lado1:number, lado2:number, lado3:number) => {
-    let resT=0;
-    resT=lado1+lado2+lado3
-    return resT
-}
-
-const areaTriangulo = (base:number, altura:number) => {
-    let resTr=0
-    resTr=(base*altura)/2
-    return resTr
-}
-
-/*Circulo */
-
-const perimetroCirculo = (radio:number) => {
-    let resCi=0
-    resCi=2*radio*Math.PI
-    return resCi
-}
-
-const areaCirculo = (radio:number) => {
-    let res=0
-    res=(radio*radio)*Math.PI
-    return res
-}
--- views/menus.ts
+++ views/menus.ts
@@ -4,11 +4,10 @@ import { leerTeclado } from './entradateclado'
 export const menu = async () => {
     let n: number
     console.log('\n')
-    console.log('1.- Rectángulo')
+    console.log('1.- Rombo')
     console.log('2.- Cuadrado')
-    console.log('3.- Triángulo')
-    console.log('4.- Círculo')
+    console.log('3.- Rectángulo')
     console.log('0.- Salir')
-    n = parseInt( await leerTeclado('opción: ') )
+    n = parseInt( await leerTeclado('Elección: ') )
     return n
 }
